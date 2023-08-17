c0a16eafc2cb95d97cc8d2bd560cf138d2f01b39
NipunGandhi <nipungandhi21@gmail.com>
2023-08-17 17:59:30 +0530

    5 questions of DP done

--- Dynamic Programming/5) Maximum Sum Of Non Adjacent Element/Maximum sum of non-adjacent elements (All code).cpp
+++ Dynamic Programming/5) Maximum Sum Of Non Adjacent Element/Maximum sum of non-adjacent elements (All code).cpp
@@ -23,11 +23,7 @@ public:
         return memo[n] = max(picked,notpicked);
     }
 
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        // return recursion(nums.size()-1, nums);
-        // vector<int> memo(nums.size(), -1);
-        // return mem(nums.size()-1, nums, memo);
+    int tab(int n,vector<int>& nums){
         if(n== 1) return nums[0];
         vector<int> dp(n, -1);
         dp[0] = nums[0];
@@ -38,5 +34,27 @@ public:
             dp[i] = max(picked,notpicked);
         }
         return dp[n-1];
+    }
+
+    int space(int n,vector<int>& nums){
+        if(n== 1) return nums[0];
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+        for(int i = 2; i<n; i++){
+            int picked = prev2 + nums[i];
+            int notpicked = prev1 + 0;
+            prev2 = prev1;
+            prev1 = max(picked,notpicked);
+        }
+        return prev1;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        // return recursion(nums.size()-1, nums);
+        // vector<int> memo(nums.size(), -1);
+        // return mem(nums.size()-1, nums, memo);
+        // return tab(n, nums);
+        return space(n,nums);
     }
 };
